27f5be6857bdc376b7342532b47d3d59740fbf23
AkshayLon <68876839+AkshayLon@users.noreply.github.com>
2024-07-23 12:06:07 +0100

    Add Testing Dataset

--- generate_data.py
+++ generate_data.py
@@ -4,7 +4,7 @@ from PIL import Image
 import os, torch
 import numpy as np
 
-class ImageDataset(Dataset):
+class TrainingDataset(Dataset):
 
     def __init__(self):
         current_dir = os.path.dirname(__file__)
@@ -30,8 +30,8 @@ class ImageDataset(Dataset):
                     bounding_boxes.append([-1,-1,-1,-1])
                 self.x.append(x)
                 self.y.append(bounding_boxes)
-        self.x = torch.from_numpy(np.array(self.x))
-        self.y = torch.from_numpy(np.array(self.y))
+        self.x = torch.from_numpy(np.array(self.x)[:58])
+        self.y = torch.from_numpy(np.array(self.y)[:58])
     
     def __getitem__(self, index):
         return self.x[index], self.y[index]
@@ -39,5 +39,37 @@ class ImageDataset(Dataset):
     def __len__(self):
         return len(self.x)
         
+class TestingDataset(Dataset):
 
-        
+    def __init__(self):
+        current_dir = os.path.dirname(__file__)
+        data_dir = os.path.join(current_dir, "exposure_5000_cam4")
+        training_reference = dict()
+        transform = transforms.Compose([
+            transforms.Grayscale(num_output_channels=3),
+            transforms.ToTensor(),
+            transforms.Resize((448,448))
+        ])
+        for f in os.listdir(data_dir):
+            if f.endswith("png"):
+                img = Image.open(os.path.join(data_dir, f))
+                transformed_img = transform(img)
+                training_reference[transformed_img] = f[:len(f)-4]
+        self.x, self.y = list(), list()
+        for x in training_reference:
+            with open(file=os.path.join(data_dir, f"{training_reference[x]}.txt")) as f:
+                data = f.read()
+                split_boxes = data.split("\n")
+                bounding_boxes = list(list(float(n) for n in box.split(" ")[1:]) for box in split_boxes if len(box)>0)
+                if len(bounding_boxes)==1:
+                    bounding_boxes.append([-1,-1,-1,-1])
+                self.x.append(x)
+                self.y.append(bounding_boxes)
+        self.x = torch.from_numpy(np.array(self.x)[58:])
+        self.y = torch.from_numpy(np.array(self.y)[58:])
+    
+    def __getitem__(self, index):
+        return self.x[index], self.y[index]
+    
+    def __len__(self):
+        return len(self.x)       
